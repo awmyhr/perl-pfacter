1da2fdf5ca5106adaf5bbe3a226ceed670997a39
awmyhr <awmyhr@gmail.com>
2014-06-09 16:08:54 -0400

    work begins on 2.x

--- lib/Pfacter.pm
+++ lib/Pfacter.pm
@@ -5,38 +5,62 @@ our $VERSION = '1.14';
 sub modulelist {
     my $self   = shift;
     my $kernel = shift;
+    my $ver1   = shift;
+    my @modules;
 
-    my @modules = qw(
+    if ( $ver1 == 1 ) { 
+        push @modules, qw(
+
+            architecture
+            disk
+            domain
+            filesystems
+            fqdn
+            hardwaremanufacturer
+            hardwaremodel
+            hardwareplatform
+            hardwareproduct
+            hostname
+            ipaddress
+            kernel
+            kernelrelease
+            kernelversion
+            localtime
+            macaddress
+            memory
+            memorytotal
+            netmask
+            operatingsystem
+            processor
+            processorcount
+            productid
+            serialnumber
+            swap
+            uniqueid
+        );
+    } else {
+        push @modules, qw(
+
+            architecture
+            domain
+            filesystems
+            fqdn
+            hardwaremodel
+            hostname
+            ipaddress
+            kernel
+            kernelrelease
+            kernelversion
+            macaddress
+            netmask
+            operatingsystem
+            processor
+            processorcount
+            serialnumber
+            uniqueid
+        );
+    }
     
-        architecture
-        disk
-        domain
-        filesystems
-        fqdn
-        hardwaremanufacturer
-        hardwaremodel
-        hardwareplatform
-        hardwareproduct
-        hostname
-        ipaddress
-        kernel
-        kernelrelease
-        kernelversion
-        localtime
-        macaddress
-        memory
-        memorytotal
-        netmask
-        operatingsystem
-        processor
-        processorcount
-        productid
-        serialnumber
-        swap
-        uniqueid
-
-    );
-
     # Kernel-specific
     for ( $kernel ) {
         /Linux/ && do {
